fd109fd15ea9c1bbb87ffe3f24a3b2ef62106169
suzhiwu <suzhiwu@kuaishou.com>
2020-01-01 20:26:59 +0800

    add interrupt demo

--- src/main/java/cn/dyz/tools/file/concurrent/threadlearn/interupt/InterruptDemo.java
+++ src/main/java/cn/dyz/tools/file/concurrent/threadlearn/interupt/InterruptDemo.java
@@ -5,7 +5,37 @@ package cn.dyz.tools.file.concurrent.threadlearn.interupt;
  */
 public class InterruptDemo {
 
+//    public static void main(String[] args) throws InterruptedException {
+//        Thread t1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    Thread.sleep(10);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+////                while (!Thread.currentThread().isInterrupted()) {
+////                    System.out.println(Thread.currentThread() + ":" + "hello");
+////                }
+//            }
+//        });
+//
+//        t1.start();
+//        t1.interrupt();
+//        System.out.println("main is over");
+//    }
+
     public static void main(String[] args) {
+        Thread threadOne = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (;;);
+            }
+        });
+
+        threadOne.start();
+        threadOne.interrupt();
+        System.out.println("thread: " + threadOne.isInterrupted());
 
     }
 }
