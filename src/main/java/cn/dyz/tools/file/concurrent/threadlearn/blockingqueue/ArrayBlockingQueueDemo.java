387337f9f791746b35b6392af9f5934d77ac0369
suzhiwu <suzhiwu@kuaishou.com>
2020-01-05 15:46:39 +0800

    add LinkedBlockingQueue

--- src/main/java/cn/dyz/tools/file/concurrent/threadlearn/blockingqueue/ArrayBlockingQueueDemo.java
+++ src/main/java/cn/dyz/tools/file/concurrent/threadlearn/blockingqueue/ArrayBlockingQueueDemo.java
@@ -1,6 +1,7 @@
 package cn.dyz.tools.file.concurrent.threadlearn.blockingqueue;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Create by suzhiwu on 2020/1/3
@@ -8,26 +9,32 @@ import java.util.concurrent.ArrayBlockingQueue;
 public class ArrayBlockingQueueDemo {
 
     public static void main(String[] args) throws InterruptedException {
-        ArrayBlockingQueue<Integer> abq = new ArrayBlockingQueue<Integer>(1);
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    abq.put(3);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    abq.put(4);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(abq);
-            }
-        });
-        t1.start();
-        Thread.sleep(1000);
-        t1.interrupt();
+//        ArrayBlockingQueue<Integer> abq = new ArrayBlockingQueue<Integer>(1);
+//        Thread t1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    abq.put(3);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    abq.put(4);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println(abq);
+//            }
+//        });
+//        t1.start();
+//        Thread.sleep(1000);
+//        t1.interrupt();
+
+        LinkedBlockingQueue<String> lbq = new LinkedBlockingQueue<>(2);
+        lbq.offer("1");
+        lbq.offer("2");
+        lbq.offer("3");
+        System.out.println(lbq);
 
     }
 }
