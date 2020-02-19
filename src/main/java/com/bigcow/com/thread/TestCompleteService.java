0b2bef08c42969d95438717ddd5cbf4ea68a099f
suzhiwu <suzhiwu@kuaishou.com>
2020-02-19 09:23:04 +0800

    complete service

---
+++ src/main/java/com/bigcow/com/thread/TestCompleteService.java
@@ -0,0 +1,36 @@
+package com.bigcow.com.thread;
+
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * create by suzhiwu on 2019/02/18
+ */
+public class TestCompleteService {
+    /**
+     * complete service 让提交的runnable 或者 callback有序完成
+     * @param args
+     */
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        CompletionService service = new ExecutorCompletionService(Executors.newFixedThreadPool(2));
+        for (int i = 0; i < 5; i++) {
+            if (i == 3) {
+                service.submit(() -> {
+                    Thread.sleep(2000);
+                    return 2;
+                });
+            } else {
+                service.submit(() -> {
+                    return 1;
+                });
+            }
+        }
+        while (true) {
+            Future take = service.take();
+            System.out.println(take.get());
+        }
+    }
+}
