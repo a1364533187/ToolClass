15749fd12a65aaf35f173719df3c767bba3cd101
suzhiwu <suzhiwu@kuaishou.com>
2020-03-13 20:19:01 +0800

    add schedule pool

--- src/main/java/cn/dyz/tools/file/schedulepool/SchedulePoolTest.java
+++ src/main/java/cn/dyz/tools/file/schedulepool/SchedulePoolTest.java
@@ -15,25 +15,55 @@ public class SchedulePoolTest {
      */
     public static void main(String[] args) {
         ScheduledExecutorService service = Executors.newScheduledThreadPool(10);
+//        service.scheduleWithFixedDelay(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                try {
+//                    Thread.sleep(6000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("job1" + "---" + System.currentTimeMillis() / 1000);
+//            }
+//        }, 5, 5, TimeUnit.SECONDS);
+
+        //都不会并发的执行
+        /**
+         * scheduleWithFixedDelay
+         *
+         * 如果delay设置的是3秒，系统执行要5秒；中间delay3s, 下一个任务也需要等在也就是任务与任务之间的差异是8s；
+         *
+         * 如果delay设置的是3s，系统执行要2s；那么需要等到3S后再次执行下一次任务。
+         */
         service.scheduleWithFixedDelay(new Runnable() {
 
             @Override
             public void run() {
                 try {
-                    Thread.sleep(6000);
+                    Thread.sleep(10000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("job1" + "---" + System.currentTimeMillis() / 1000);
+                System.out.println("job2" + "---" + System.currentTimeMillis() / 1000);
             }
-        }, 5, 5, TimeUnit.SECONDS);
+        }, 3, 3, TimeUnit.SECONDS);
 
-        service.scheduleWithFixedDelay(new Runnable() {
+
+        /**
+         * scheduleAtFixedRate
+         * period为固定周期时间，按照一定频率来重复执行任务。
+         *
+         * 如果period设置的是3秒，系统执行要5秒；那么等上一次任务执行完就立即执行，也就是任务与任务之间的差异是5s；
+         *
+         * 如果period设置的是3s，系统执行要2s；那么需要等到3S后再次执行下一次任务。
+         */
+        service.scheduleAtFixedRate(new Runnable() {
 
             @Override
             public void run() {
                 try {
-                    Thread.sleep(4000);
+                    Thread.sleep(10000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
