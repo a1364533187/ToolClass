999550cd3174e7f05398ef1be39e56901da8b381
suzhiwu <suzhiwu@kuaishou.com>
2021-01-05 11:39:05 +0800

    add map test

--- src/test/java/cn/dyz/tools/DruidTest.java
+++ src/test/java/cn/dyz/tools/DruidTest.java
@@ -1,5 +1,7 @@
 package cn.dyz.tools;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
@@ -14,5 +16,13 @@ public class DruidTest {
         SqlParser.Config config = SqlParser.configBuilder().build();
         SqlParser parser = SqlParser.create(sql, config);
         SqlNode node = parser.parseQuery();
+
+        ConcurrentHashMap<String, Integer> map = new ConcurrentHashMap<>();
+        map.put("3", 3);
+
+        final String a = map.toString();
+        System.out.println("a: " + a);
+        map.clear();
+        System.out.println("a:" + a);
     }
 }
